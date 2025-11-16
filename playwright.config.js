6f625bc5ca70a5f3fe40e4c7953897c9ab52542f
Tasmia Khan <106498647+KTasmi@users.noreply.github.com>
2025-11-16 09:54:35 +0600

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,14 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  retries: 0,
+  reporter: [['list'], ['html']],
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 10 * 1000,
+    ignoreHTTPSErrors: true
+  },
+});
